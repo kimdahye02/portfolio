54fbb3f59da9e302ae100ad8ebbfbb1d144843b4
kimdahye02 <76405634+kimdahye02@users.noreply.github.com>
2025-02-01 04:04:33 +0900

    modified:   index.html 	modified:   js/common.js 	modified:   css/styles.css

--- js/common.js
+++ js/common.js
@@ -7,15 +7,15 @@ $(function(){
     });
 
     // 컨텐츠 더보기
-    $(".js-load").slice(0, 4).show(); // select the first 4
-    $(".moreBtn").click(function(e){ // click event for load more
-        e.preventDefault();
-        $(".js-load:hidden").slice(0, 4).show(); // select next 4 hidden divs and show them
-        if($(".js-load:hidden").length == 0){ // check if any hidden divs still exist
-            // alert("No more divs"); // alert if there are none left
-            $('.moreBtn').hide();
-        }
-    });
+    // $(".js-load").slice(0, 4).show(); // select the first 4
+    // $(".moreBtn").click(function(e){ // click event for load more
+    //     e.preventDefault();
+    //     $(".js-load:hidden").slice(0, 4).show(); // select next 4 hidden divs and show them
+    //     if($(".js-load:hidden").length == 0){ // check if any hidden divs still exist
+    //         // alert("No more divs"); // alert if there are none left
+    //         $('.moreBtn').hide();
+    //     }
+    // });
 
     //서브 sns 탭메뉴
     $('.tabcontent > div').hide();
